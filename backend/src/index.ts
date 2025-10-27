6a0e7c6ef25ecd542f87c4f75c6c95f0128c79f5
Fano435 <108521943+Fano435@users.noreply.github.com>
2025-10-27 17:21:24 +0100

    Download working, need to change filename

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,6 @@ const port = 3000;
 
 app.use(cors());
 
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
--- backend/src/routes/convert.ts
+++ backend/src/routes/convert.ts
@@ -1,7 +1,8 @@
-// src/routes/convert.ts
 import { Router } from "express";
 import ytdl from "ytdl-core";
 import { spawn } from "child_process";
+import fs from "fs";
+import path from "path";
 
 const router = Router();
 
@@ -10,44 +11,35 @@ router.get("/", async (req, res) => {
     console.log("URL reçue :", videoUrl)
 
     if (!videoUrl || !ytdl.validateURL(videoUrl)) {
-        return res.status(400).send("Missing YouTube URL");
+        return res.status(400).send("Missing or unvalid YouTube URL");
     }
+    
+    const outputFile = path.join("/tmp", `audio_${Date.now()}.mp3`)
 
-    // const info = await ytdl.getInfo(videoUrl);
-    // const title = info.videoDetails.title
-    // res.header("Content-Disposition", `attachment; filename="${title}.mp3"`);
-    res.header("Content-Disposition", 'attachment; filename="audio.mp3"');
-    res.header("Content-Type", "audio/mpeg");
-
+    // Lancement du child process yt-dlp
     const ytdlp = spawn("yt-dlp", [
     "-f", "bestaudio",
     "-x", "--audio-format", "mp3",
-    "-o", "-", // output to stdout
+    "-o", outputFile,
     videoUrl
     ])
 
-    ytdlp.stdout.pipe(res)
+     // On log les erreurs de yt-dlp
+    ytdlp.stderr.on("data", data => { console.error("yt-dlp:", data.toString()); })
 
-    ytdlp.stderr.on("data", data => {
-    console.error("yt-dlp:", data.toString()); })
-  
-    ytdlp.on("error", err => {
-    console.error("Erreur yt-dlp:", err);
-    if (!res.headersSent)
-      res.status(500).json({ error: "Erreur lors de la conversion" });
+    // Quand yt-dlp se termine
+    ytdlp.on("close", code => {
+    if (code === 0) {
+        // conversion terminée → on envoie le fichier au client
+        res.download(outputFile, "audio.mp3", err => {
+        if (err) console.error("Erreur d’envoi:", err);
+        fs.unlink(outputFile, () => {}); // suppression du fichier temporaire
+      });
+    } 
+    else {
+        res.status(500).json({ error: "Échec de la conversion" });
+    }
   });
-    // let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-    // console.log(audioFormats[0])
-
-    // ffmpeg(ytdl(videoUrl, { filter: "audioonly" }))
-    // .audioBitrate(192)
-    // .format("mp3")
-    // .on("error", err => {
-    //     console.error(err);
-    //     if (!res.headersSent)
-    //       res.status(500).json({ error: "Erreur pendant la conversion audio" });
-    //   })
-    // .pipe(res);
 
 });
 
